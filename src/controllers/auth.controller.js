ce8ffda1cba7d9345ed568504b2e2b00a0c2f9b5
Sudhanshu Bagde <sudhanshubagde1234@gmail.com>
2026-02-15 22:21:57 +0530

    Verify Email

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -161,12 +161,47 @@ const currentUser = asyncHandler(async (req, res) => {
     return res
         .status(200)
         .json(
-            new ApiResponse(
-                200,
-                req.user,
-                "Current User Fetched Successfully"
-            )
-        )
-})
+            new ApiResponse(200, req.user, "Current User Fetched Successfully"),
+        );
+});
 
-export { registerUser, login, logOutUser, currentUser };
+const verifyEmail = asyncHandler(async (req, res) => {
+    const { verificationToken } = req.params;
+    if(!verificationToken){
+        throw new ApiError(400, "Email verification token is missing!");
+    }
+
+    let hashedToken = crypto
+        .createHash("sha256")
+        .update(verificationToken)
+        .digest("hex")
+    
+        const user = await User.findOne({
+            emailVerificationToken: hashedToken,
+            emailVerificationExpiry: {$gt:Date.now()}
+        })
+
+        if(!user){
+            throw new ApiError(400, "Token is invalid or expired")
+        }
+
+        emailVerificationToken = undefined;
+        emailVerificationExpiry = undefined;
+
+        user.isEmailVerified = true;
+        await user.save({validateBeforeSave: false})
+
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    {
+                        isEmailVerified: true
+                    },
+                    "Email is verified"
+                )
+            )
+});
+
+export { registerUser, login, logOutUser, currentUser, verifyEmail };
